753792fe69d34cf4383315b77695648046079b5b
knockknockyoo <iheaven0129@gmail.com>
2022-06-03 17:41:15 -0400

    Added NIH Login Service

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const {importHTML} = require("./view/import-html");
 const cors = require('cors');
 const config = require('./config');
 console.log(config);
-
+// var passport = require('passport');
+var loginGov = require('./login-gov');
 const app = express();
 app.use(cors());
 // Declare All Routes
@@ -17,14 +18,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(createSession({ session_timeout: config.session_timeout }))
 app.use(importHTML(express));
 
+// app.use(passport.initialize());
+// app.use(passport.session());
+//
+// passport.serializeUser(function(user, done) { done(null, user); });
+// passport.deserializeUser(function(user, done) { done( null, user); }); // this is where you might fetch a user record from the database. see http://www.passportjs.org/docs/configure/#sessions
+
+// loginGov.configure(passport); // configure LOA1 strategy
+
 async function activatePassport() {
   return await require('./lib/passport')(app);
 }
 activatePassport().then((passport) => {
-  const authRouter = require('./routes/google-auth')(passport);
-  app.use('/api/auth', authRouter);
+    const authRouter = require('./routes/google-auth')(passport);
+    app.use('/api/auth', authRouter);
+    const nihRouter = require('./routes/nih-auth')(passport);
+    app.use('', nihRouter);
   // catch 404 and forward to error handler
-  app.use(throwError);
-  app.use(errorHandler);
+    app.use(throwError);
+    app.use(errorHandler);
 });
 module.exports = app;
---
+++ routes/nih-auth.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const url = require('url');
+const config = require('../config');
+const nodeFetch = require("node-fetch");
+
+module.exports = function (passport) {
+
+    // TODO ADD Custom Redirect URL
+    router.get(
+        '/nih_external_login',
+        (request, response, next) => {
+            const urlParam = {
+                client_id: config.nih.clientId,
+                redirect_uri: "http://localhost:4010/profile",
+                ...config.nih.params
+            }
+            const params = new URLSearchParams(urlParam).toString();
+            response.redirect(`${config.nih.authorizeUrl}?${params}`);
+        }
+    );
+
+    router.get(
+        '/profile',
+        (request, response, next) => {
+            const queryObject = url.parse(request.url, true).query;
+            response.redirect('/?code='+ queryObject.code);
+        }
+    );
+
+    router.get(
+        '/nih_user',
+        async (request, response, next) => {
+            const queryObject = url.parse(request.url, true).query;
+            const auth_code = queryObject.code;
+            const token = await getToken(auth_code);
+            const user = await getUserInfo(token);
+            response.send({"msg": user});
+        }
+    );
+
+    async function getUserInfo(accessToken) {
+        const response = await nodeFetch('https://stsstg.nih.gov/openid/connect/v1/userinfo', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ` + accessToken
+            }
+        });
+        const jsonResponse = await response.json();
+        return jsonResponse;
+    }
+
+    async function getToken(auth_code) {
+
+        // TODO add custom redirect url
+        const response = await nodeFetch(config.nih.tokenUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                code: auth_code,
+                redirect_uri: "http://localhost:4010/profile",
+                grant_type: "authorization_code",
+                client_id: config.nih.clientId,
+                client_secret: config.nih.clientSecret,
+                scope: "openid email profile"
+            })
+        });
+        const jsonResponse = await response.json();
+        const token = jsonResponse.access_token;
+        return token;
+    }
+
+    router.get('/nih_login', (req, res) => {
+        res.send('<a href="/auth/login_process">nih login failed</a>');
+    });
+
+    return router;
+}
