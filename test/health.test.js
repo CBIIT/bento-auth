fa272bfd5d22744b52cf7e6dab4075007ec8c5a1
knockknockyoo <iheaven0129@gmail.com>
2022-06-13 10:32:43 -0400

    Jest Test Added - Healthcheck - Google Authentication

---
+++ test/health.test.js
@@ -0,0 +1,21 @@
+const {ping, version} = require('../controllers/healthcheck')
+
+describe('Connection Test', () => {
+    const req= {};
+    const res = {
+        status: jest.fn(() => res),
+        send: jest.fn(),
+        json: jest.fn()
+    };
+    const next = jest.fn();
+
+    test('/ping', async () => {
+        await ping(req,res,next);
+        expect(res.send).toBeCalledWith('pong');
+    });
+
+    test('/version', async () => {
+        await version(req, res, next);
+        expect(res.json).toBeCalledWith({version: "1.0", date:"2022.05.19"});
+    });
+});
