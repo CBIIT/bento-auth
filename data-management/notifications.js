b2695cff9a02ffeb07a4830320badcd32970c6a7
Austin Mueller <64418315+AustinSMueller@users.noreply.github.com>
2022-06-23 15:46:51 -0400

    Minor cleanup
    Delete email.js
    Remove leftover console.log debugging functions

--- data-management/notifications.js
+++ data-management/notifications.js
@@ -1,82 +1,75 @@
 const yaml = require('js-yaml');
-const fs   = require('fs');
+const fs = require('fs');
 const {sendNotification} = require("../services/notify");
 const {createEmailTemplate} = require("../lib/create-email-template");
 
 let email_constants = undefined
-try{
+try {
     email_constants = yaml.load(fs.readFileSync('yaml/notification_email_values.yaml', 'utf8'));
-}
-catch (e) {
+} catch (e) {
     console.error(e)
 }
 
 
 module.exports = {
     sendAdminNotification: async (admins, template_params) => {
-        console.log(admins);
-        if (email_constants){
+        if (email_constants) {
             await sendNotification(
                 email_constants.NOTIFICATION_SENDER,
                 email_constants.ADMIN_NOTIFICATION_SUBJECT,
                 await createEmailTemplate("notification-template.html", template_params),
                 admins
             );
-        }
-        else{
+        } else {
             console.error("Unable to load email constants from file, email not sent")
         }
 
     },
     sendRegistrationConfirmation: async (email, template_params) => {
-        if (email_constants){
+        if (email_constants) {
             await sendNotification(
                 email_constants.NOTIFICATION_SENDER,
                 email_constants.CONFIRMATION_SUBJECT,
                 await createEmailTemplate("notification-template.html", template_params),
                 email
             );
-        }
-        else{
+        } else {
             console.error("Unable to load email constants from file, email not sent")
         }
     },
     sendApprovalNotification: async (email, template_params) => {
-        if (email_constants){
+        if (email_constants) {
             await sendNotification(
                 email_constants.NOTIFICATION_SENDER,
                 email_constants.APPROVAL_SUBJECT,
                 await createEmailTemplate("notification-template.html", template_params),
                 email
             );
-        }
-        else{
+        } else {
             console.error("Unable to load email constants from file, email not sent")
         }
     },
     sendRejectionNotification: async (email, template_params) => {
-        if (email_constants){
+        if (email_constants) {
             await sendNotification(
                 email_constants.NOTIFICATION_SENDER,
                 email_constants.REJECTION_SUBJECT,
                 await createEmailTemplate("notification-template.html", template_params),
                 email
             );
-        }
-        else{
+        } else {
             console.error("Unable to load email constants from file, email not sent")
         }
     },
     sendEditNotification: async (email, template_params) => {
-        if (email_constants){
+        if (email_constants) {
             await sendNotification(
                 email_constants.NOTIFICATION_SENDER,
                 email_constants.EDIT_SUBJECT,
                 await createEmailTemplate("notification-template.html", template_params),
                 email
             );
-        }
-        else{
+        } else {
             console.error("Unable to load email constants from file, email not sent")
         }
     }
