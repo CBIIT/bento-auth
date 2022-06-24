12a1500ce6992f97937a42bcb0050d4e421f020b
knockknockyoo <iheaven0129@gmail.com>
2022-06-24 11:26:19 -0400

    Updated Email Template TODO Rejection/Edit Reasons TODO Add Registration URL

--- data-management/notifications.js
+++ data-management/notifications.js
@@ -11,73 +11,63 @@ catch (e) {
     console.error(e)
 }
 
+function validateEmailConstants(fn) {
+    if (email_constants) return fn;
+    console.error("Unable to load email constants from file, email not sent")
+}
 
 module.exports = {
-    sendAdminNotification: async (admins, template_params) => {
+    sendAdminNotification: validateEmailConstants(async (admins, template_params) => {
         console.log(admins);
-        if (email_constants){
-            await sendNotification(
-                email_constants.NOTIFICATION_SENDER,
-                email_constants.ADMIN_NOTIFICATION_SUBJECT,
-                await createEmailTemplate("notification-template.html", template_params),
-                admins
-            );
-        }
-        else{
-            console.error("Unable to load email constants from file, email not sent")
-        }
-
-    },
-    sendRegistrationConfirmation: async (email, template_params) => {
-        if (email_constants){
-            await sendNotification(
-                email_constants.NOTIFICATION_SENDER,
-                email_constants.CONFIRMATION_SUBJECT,
-                await createEmailTemplate("notification-template.html", template_params),
-                email
-            );
-        }
-        else{
-            console.error("Unable to load email constants from file, email not sent")
-        }
-    },
-    sendApprovalNotification: async (email, template_params) => {
-        if (email_constants){
-            await sendNotification(
-                email_constants.NOTIFICATION_SENDER,
-                email_constants.APPROVAL_SUBJECT,
-                await createEmailTemplate("notification-template.html", template_params),
-                email
-            );
-        }
-        else{
-            console.error("Unable to load email constants from file, email not sent")
-        }
-    },
-    sendRejectionNotification: async (email, template_params) => {
-        if (email_constants){
-            await sendNotification(
-                email_constants.NOTIFICATION_SENDER,
-                email_constants.REJECTION_SUBJECT,
-                await createEmailTemplate("notification-template.html", template_params),
-                email
-            );
-        }
-        else{
-            console.error("Unable to load email constants from file, email not sent")
-        }
-    },
-    sendEditNotification: async (email, template_params) => {
-        if (email_constants){
-            await sendNotification(
-                email_constants.NOTIFICATION_SENDER,
-                email_constants.EDIT_SUBJECT,
-                await createEmailTemplate("notification-template.html", template_params),
-                email
-            );
-        }
-        else{
-            console.error("Unable to load email constants from file, email not sent")
-        }
-    }
+        await sendNotification(
+            email_constants.NOTIFICATION_SENDER,
+            email_constants.ADMIN_NOTIFICATION_SUBJECT,
+            await createEmailTemplate("notification-template.html", {
+                message: email_constants.ADMIN_NOTIFICATION_CONTENT, title: email_constants.ADMIN_NOTIFICATION_SUBJECT, ...template_params
+            }),
+            admins
+        );
+    }),
+    sendRegistrationConfirmation: validateEmailConstants(async (email, template_params) => {
+        await sendNotification(
+            email_constants.NOTIFICATION_SENDER,
+            email_constants.CONFIRMATION_SUBJECT,
+            await createEmailTemplate("notification-template.html", {
+                message: email_constants.CONFIRMATION_CONTENT, title: email_constants.CONFIRMATION_SUBJECT, ...template_params
+            }),
+            email
+        );
+    }),
+    sendApprovalNotification: validateEmailConstants(async (email, template_params) => {
+        await sendNotification(
+            email_constants.NOTIFICATION_SENDER,
+            email_constants.APPROVAL_SUBJECT,
+            await createEmailTemplate("notification-template.html", {
+                message: email_constants.APPROVAL_CONTENT, title: email_constants.APPROVAL_SUBJECT, ...template_params
+            }),
+            email
+        );
+    }),
+    sendRejectionNotification: validateEmailConstants(async (email, template_params) => {
+        await sendNotification(
+            email_constants.NOTIFICATION_SENDER,
+            email_constants.REJECTION_SUBJECT,
+            await createEmailTemplate("notification-template.html", {
+                // @Austin TODO Adding following rejection reason
+                message: email_constants.REJECTION_CONTENT_PRE_COMMENT + email_constants.REJECTION_CONTENT_POST_COMMENT, title: email_constants.REJECTION_SUBJECT, ...template_params
+            }),
+            email
+        );
+    }),
+    sendEditNotification: validateEmailConstants(async (email, template_params) => {
+        await sendNotification(
+            email_constants.NOTIFICATION_SENDER,
+            email_constants.EDIT_SUBJECT,
+            await createEmailTemplate("notification-template.html", {
+                // @Austin TODO Adding following rejection reason
+                message: email_constants.EDIT_CONTENT_PRE_COMMENT + email_constants.EDIT_CONTENT_POST_COMMENT, title: email_constants.EDIT_SUBJECT, ...template_params
+            }),
+            email
+        );
+    })
 }
