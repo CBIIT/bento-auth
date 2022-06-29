007d7b9918fbd290357c312e0646f475a5f69f21
knockknockyoo <iheaven0129@gmail.com>
2022-06-29 14:45:48 -0400

    Default IDP - enabled if idp is not present Default redirect url - enabled if redirect url is not provided from the client

--- config.js
+++ config.js
@@ -5,9 +5,7 @@ const config = {
   version: process.env.VERSION,
   date: process.env.DATE,
   idp: process.env.IDP ? process.env.IDP.toLowerCase() : 'google',
-  client_id: process.env.CLIENT_ID,
-  client_secret: process.env.CLIENT_SECRET,
-  redirect_url: process.env.REDIRECT_URL,
+  redirectUri: process.env.GOOGLE_REDIRECT_URL ? process.env.GOOGLE_REDIRECT_URL : 'http://localhost:4010',
   cookie_secret: process.env.COOKIE_SECRET,
   session_timeout: process.env.SESSION_TIMEOUT ? parseInt(process.env.SESSION_TIMEOUT) : 30 * 60,  // 30 minutes
   authorization_enabled: process.env.AUTHORIZATION_ENABLED ? process.env.AUTHORIZATION_ENABLED.toLowerCase() === 'true' : true,
