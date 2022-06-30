c71ef8e49822484f5c4fb957425644ccf4c847dc
knockknockyoo <iheaven0129@gmail.com>
2022-06-30 11:41:44 -0400

    user-constant updated

--- constants/user-constant.js
+++ constants/user-constant.js
@@ -2,10 +2,14 @@
 const APPROVED = 'approved';
 const REJECTED = 'rejected';
 const REGISTERED = 'registered';
+
 const INITIALIZED = 'initialized'
+const ACTIVE = 'active';
+const DISABLED = 'disabled';
+const DELETED = 'deleted';
 
 // User Role
 const STANDARD = 'standard';
 const ADMIN = 'admin';
 
-module.exports = {APPROVED, INITIALIZED, STANDARD, ADMIN, REJECTED, REGISTERED}
+module.exports = {APPROVED, INITIALIZED, STANDARD, ADMIN, REJECTED, REGISTERED, ACTIVE, DISABLED, DELETED}
