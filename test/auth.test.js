d8aaad052abdfa902d3e43ffe53e809110685ce2
knockknockyoo <iheaven0129@gmail.com>
2022-07-12 16:14:07 -0400

    logout and logout test codes

---
+++ test/auth.test.js
@@ -0,0 +1,32 @@
+const app = require('../app');
+const config = require("../config");
+const request = require('supertest');
+const nihClient = require("../idps/nih");
+const {NIH, LOGIN_GOV} = require("../constants/idp-constants");
+jest.mock("../idps/nih");
+describe('GET /auth test', ()=> {
+    const LOGOUT_ROUTE = '/api/auth/logout';
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test(`auth logout nih`, async () => {
+        const nihClient = require('../idps/nih');
+        nihClient.logout.mockReturnValue(Promise.resolve());
+        await request(app)
+            .post(LOGOUT_ROUTE)
+            .send({IDP: NIH})
+            .expect(200);
+        expect(nihClient.logout).toBeCalledTimes(1);
+    });
+
+    test(`auth logout login.gov`, async () => {
+        const nihClient = require('../idps/nih');
+        nihClient.logout.mockReturnValue(Promise.resolve());
+        await request(app)
+            .post(LOGOUT_ROUTE)
+            .send({IDP: LOGIN_GOV})
+            .expect(200);
+        expect(nihClient.logout).toBeCalledTimes(1);
+    });
+});
