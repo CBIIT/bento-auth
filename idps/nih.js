15e9affdcefa9af74e4e7c6bb97058fd60d586b1
knockknockyoo <iheaven0129@gmail.com>
2022-07-21 15:13:46 -0400

    added an invalid login error if NIH login failed

--- idps/nih.js
+++ idps/nih.js
@@ -1,12 +1,16 @@
 const {getNIHToken, nihUserInfo, nihLogout, getIDP} = require("../services/nih-auth");
 const client = {
     login: async (code, redirectingURL) => {
-        const token = await getNIHToken(code, redirectingURL);
-        const user = await nihUserInfo(token);
-        // use a preferred name or email as identity
-        const idp = getIDP(user['preferred_username'] ? user['preferred_username'] : user.email);
-        // Leave as blank if no name exits
-        return {name: user.first_name ? user.first_name: '', lastName: user.last_name ? user.last_name: '', email: user.email, tokens: token, idp: idp};
+        try {
+            const token = await getNIHToken(code, redirectingURL);
+            const user = await nihUserInfo(token);
+            // use a preferred name or email as identity
+            const idp = getIDP(user['preferred_username'] ? user['preferred_username'] : user.email);
+            // Leave as blank if no name exits
+            return {name: user.first_name ? user.first_name: '', lastName: user.last_name ? user.last_name: '', email: user.email, tokens: token, idp: idp};
+        } catch {
+            throw new Error("Invalid access NIH token or unauthorized NIH login attempt");
+        }
     },
     authenticated: async (tokens) => {
         try {
