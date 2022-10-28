1afb49183b13a80c36e6a1fe12b00f34e2a23fb6
Austin Mueller <64418315+AustinSMueller@users.noreply.github.com>
2022-10-28 12:39:04 -0400

    Address comments from review
    - Removed response creation methods and calls with null parameters
    - Added else block to with "internal server error" message, this should not be reachable but will return a response in the event of an unexpected edge case

--- services/mysql-connection.js
+++ services/mysql-connection.js
@@ -13,8 +13,7 @@ const getTTL = (req, res) => {
         const sessionID = getSessionIDFromCookie(req, res);
         if (err) {
             console.log(err);
-            let response = createTTLErrorResponse("Could not establish a connection to the session database, see logs for details");
-            res.json(response);
+            res.json({ttl: null, error: "Could not establish a connection to the session database, see logs for details"});
             return;
         }
         else if (sessionID){
@@ -22,44 +21,30 @@ const getTTL = (req, res) => {
                 let response;
                 if (err){
                     console.log(err);
-                    response = createTTLErrorResponse("An error occurred while querying the database, see logs for details");
+                    response = {ttl: null, error: "An error occurred while querying the database, see logs for details"};
                 }
                 else if (!rows || !rows[0] || !rows[0].expires){
-                    response = createTTLResponse(0);
+                    response = {ttl: 0};
                 }
                 else{
                     let expires = rows[0].expires;
                     let dt = new Date(expires * 1000);
                     let ttl = Math.round((dt.valueOf() - Date.now())/1000);
-                    response = createTTLResponse(ttl);
+                    response = {ttl: ttl};
                 }
                 res.json(response);
             });
+        }
+        else {
+            res.json({ttl: null, error: "An internal server error occurred, please contact the administrators"});
         }
         currentConnection.release();
     });
 }
 
-function createTTLResponse(ttl){
-    return createTTLResponse(ttl, null);
-}
-
-function createTTLErrorResponse(error){
-    return createTTLResponse(null, error);
-}
-
-function createTTLResponse(ttl, error){
-    const response = {ttl: ttl};
-    if (error){
-        response.error = error;
-    }
-    return response;
-}
-
 function getSessionIDFromCookie(req, res){
     if (!req || !req.cookies || !req.cookies["connect.sid"]){
-        const response = createTTLResponse(0);
-        res.json(response);
+        res.json({ttl: 0});
         return null;
     }
     else{
